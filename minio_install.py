e71564ee1c3bf0019af51c9cf39bd290c4785895
PWZeil <144216088+PWZeil@users.noreply.github.com>
2025-06-06 16:33:30 +0200

    Update minio_install.py

--- minio_install.py
+++ minio_install.py
@@ -14,19 +14,14 @@ def build(folder_installation="./", global_env={}):
 
     # Fixed username
     minio_user = "admin"
-
-    # Generate MinIO password if not provided
-    minio_pass = global_env.get("IECON_INTELLIGENCE_MINIO_PASSWORD", "")
-    if not minio_pass:
-        print("INFO: Generate minio_pass password")
-        minio_pass = generate_random_password()
+    minio_pass = generate_random_password()
 
     # Write .env file with MinIO user and password
     env_path = os.path.join(folder_installation, ".env")
     print(f"Writing MinIO credentials to {env_path}")
     with open(env_path, "w") as fw:
-        fw.write(f'IECON_INTELLIGENCE_MINIO_USER="{minio_user}"\n')
-        fw.write(f'IECON_INTELLIGENCE_MINIO_PASSWORD="{minio_pass}"\n')
+        fw.write(f'MINIO_ROOT_USER="{minio_user}"\n')
+        fw.write(f'MINIO_ROOT_PASSWORD="{minio_pass}"\n')
         fw.flush()
 
     print("Done")
