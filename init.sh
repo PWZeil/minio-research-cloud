78193c59fa5ffe616a563bf39fa604a0cbbaa951
PWZeil <144216088+PWZeil@users.noreply.github.com>
2025-06-06 17:45:40 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,13 +6,14 @@ python3 minio_install.py
 # Define the Nginx configuration file path
 nginx_conf="/etc/nginx/conf.d/ssl_main.conf"
 
-# Replace root directive with proxy for /
-sed -i 's|root /var/www/html;|location / {\n    \tproxy_pass http://localhost:8080;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    }|' "$nginx_conf"
+# Remove root and index directives (if they exist)
+sed -i '/root \/var\/www\/html;/d' "$nginx_conf"
+sed -i '/index index.html index.htm;/d' "$nginx_conf"
 
-# Remove index directive
-sed -i 's|index index.html index.htm;||' "$nginx_conf"
+# Add /admin proxy block
+sed -i '$a location /admin/ {\n\tproxy_pass http://localhost:8080/;\n\tproxy_set_header Host $host;\n\tproxy_set_header X-Real-IP $remote_addr;\n\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\tproxy_set_header X-Forwarded-Proto $scheme;\n}' "$nginx_conf"
 
-# Add /api  proxy block
+# Add /api proxy block
 sed -i '$a location /api/ {\n\tproxy_pass http://localhost:8081/;\n\tproxy_set_header Host $host;\n\tproxy_set_header X-Real-IP $remote_addr;\n\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\tproxy_set_header X-Forwarded-Proto $scheme;\n}' "$nginx_conf"
 
 # Restart nginx to apply changes
