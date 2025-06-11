9041d17c0a31470e1a7783c5026e7f1b75bf8a03
PWZeil <144216088+PWZeil@users.noreply.github.com>
2025-06-11 14:44:31 +0200

    Update minio_install.py

--- minio_install.py
+++ minio_install.py
@@ -5,11 +5,12 @@
 import random
 import string
 import os
+import argparse
 
 
-def build(folder_installation="./", global_env={}):
+def build(folder_installation="./", redirect_url=None):
     """
-        Create MinIO admin user and generate password only
+    Create MinIO admin user and generate password, optionally include redirect URL.
     """
 
     # Fixed username
@@ -18,10 +19,12 @@ def build(folder_installation="./", global_env={}):
 
     # Write .env file with MinIO user and password
     env_path = os.path.join(folder_installation, ".env")
-    print(f"Writing MinIO credentials to {env_path}")
+    print(f"Writing MinIO configuration to {env_path}")
     with open(env_path, "w") as fw:
         fw.write(f'MINIO_ROOT_USER="{minio_user}"\n')
         fw.write(f'MINIO_ROOT_PASSWORD="{minio_pass}"\n')
+        if redirect_url:
+            fw.write(f'MINIO_BROWSER_REDIRECT_URL="{redirect_url}"\n')
         fw.flush()
 
     print("Done")
@@ -36,4 +39,8 @@ def generate_random_password(length=20):
 
 
 if __name__ == "__main__":
-    build()
+    parser = argparse.ArgumentParser(description="MinIO installation environment generator")
+    parser.add_argument("--MINIO_BROWSER_REDIRECT_URL", type=str, help="URL to use for the MinIO console", default=None)
+    args = parser.parse_args()
+
+    build(redirect_url=args.MINIO_BROWSER_REDIRECT_URL)
